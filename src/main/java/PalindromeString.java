a6833ffcfade31d600ca3d190b575993ff116071
Rakesh Gourineni <grakeshrao@gmail.com>
2016-08-18 14:44:06 -0400

    Logic for Palindrome String

---
+++ src/main/java/PalindromeString.java
@@ -0,0 +1,23 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by rakeshg on 7/28/16.
+ */
+public class PalindromeString {
+
+    public static void main (String[] args) {
+        String a = "abc";
+        String b = "eee";
+
+        StringBuilder ab = new StringBuilder(a);
+
+        a = ab.reverse().toString();
+
+        if (StringUtils.equalsIgnoreCase(a,b)) {
+            System.out.println(1);
+        } else {
+            System.out.println(-1);
+        }
+
+    }
+}
