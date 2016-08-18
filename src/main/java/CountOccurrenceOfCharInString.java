bc9a61a3c3d488e50ee5f57a81fd56afc039f985
Rakesh Gourineni <grakeshrao@gmail.com>
2016-08-18 14:42:54 -0400

    Count the occurrence of the characters in String

--- src/main/java/CountOccurrenceOfCharInString.java
+++ src/main/java/CountOccurrenceOfCharInString.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Map;
 
 /**
@@ -5,18 +7,22 @@ import java.util.Map;
  */
 public class CountOccurrenceOfCharInString extends FirstNonRepeatedCharInString {
 
+    public static void main(String[] args) {
+        CountOccurrenceOfCharInString cn = new CountOccurrenceOfCharInString();
+        cn.countOccurrence("RakeshRao", 'r');
+    }
+
+    /**
+     * Logic for counting occurrence of the characters in String. We are reusing the splitStringToMap from FirstNonRepeatedCharInString class.
+     * @param word
+     * @param ch
+     */
     public void countOccurrence(String word, char ch) {
 
         String lowerWord = word.toLowerCase();
         Map<Character, Integer> map = super.splitStringToMap(lowerWord);
 
         System.out.println(map.get(ch));
-
-    }
-
-    public static void main(String[] args) {
-        CountOccurrenceOfCharInString cn = new CountOccurrenceOfCharInString();
-        cn.countOccurrence("RakeshRao", 'r');
 
     }
 }
