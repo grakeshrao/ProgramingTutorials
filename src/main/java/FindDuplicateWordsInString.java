7317e3e3aea72caa6fd41aac69c902d4cdf741c8
Rakesh Gourineni <grakeshrao@gmail.com>
2016-08-18 14:35:14 -0400

    Implementation for finding duplicate characters in String

---
+++ src/main/java/FindDuplicateWordsInString.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by rakeshg on 8/18/16.
+ */
+public class FindDuplicateWordsInString {
+
+    public static void main(String[] args) {
+       findDuplicateWord("rakeeshh");
+    }
+
+    /**
+     * This logic uses Set and determine the duplicate characters
+     * @param word
+     */
+    public static void findDuplicateWord(String word) {
+        Set<Character> wordSet = new HashSet<Character>();
+         int len = word.length();
+        System.out.println("The duplicate characters in the String are: ");
+        for (int i = 0; i <= word.length() - 1; i++) {
+            char ch = word.charAt(i);
+            if (!wordSet.add(ch)) {
+                System.out.println(ch);
+            }
+        }
+    }
+}
