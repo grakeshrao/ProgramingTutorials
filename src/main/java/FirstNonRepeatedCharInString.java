1b67a4fa0c641fec857c08502ab745275ad76ebc
Rakesh Gourineni <grakeshrao@gmail.com>
2016-08-18 14:31:28 -0400

    Rearranged the logic blocks

--- src/main/java/FirstNonRepeatedCharInString.java
+++ src/main/java/FirstNonRepeatedCharInString.java
@@ -6,13 +6,19 @@ import java.util.Map;
  */
 public class FirstNonRepeatedCharInString {
 
+    public static void main(String[] args) {
+        FirstNonRepeatedCharInString fn = new FirstNonRepeatedCharInString();
+        fn.firstNonRepeatedString("Infoconnect");
+    }
+
+    // Logic for finding first NonRepeated String
     public void firstNonRepeatedString(String word) {
 
         String lowerWord = word.toLowerCase();
 
         Map<Character, Integer> map = splitStringToMap(lowerWord);
 
-        for ( int i = 0; i <= lowerWord.length()-1; i++) {
+        for (int i = 0; i <= lowerWord.length() - 1; i++) {
             char letter = lowerWord.charAt(i);
             if (map.get(letter) == 1) {
                 System.out.println(letter);
@@ -24,23 +30,14 @@ public class FirstNonRepeatedCharInString {
     protected Map<Character, Integer> splitStringToMap(String word) {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
 
-        for (int i=0; i < word.length() - 1; i++) {
+        for (int i = 0; i < word.length() - 1; i++) {
             char ch = word.charAt(i);
             if (map.containsKey(ch)) {
                 map.put(ch, map.get(ch) + 1);
             } else {
-                map.put(ch,1);
+                map.put(ch, 1);
             }
         }
         return map;
     }
-
-
-    public static void main (String[] args) {
-        FirstNonRepeatedCharInString fn = new FirstNonRepeatedCharInString();
-        fn.firstNonRepeatedString("Infoconnect");
-
-    }
-
-
 }
